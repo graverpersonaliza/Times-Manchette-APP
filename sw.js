c1802054a8ba933ce33ee50573d0bfb64b7222a9
Graver Personaliza <lojagraver@gmail.com>
2026-03-23 13:08:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker */
-const CACHE_NAME = "manchette-dev-fix26-nota-primeira-inscricao";
+const CACHE_NAME = "manchette-dev-fix27-device-lock";
 const ASSETS = [
   "./",
   "./index.html",
