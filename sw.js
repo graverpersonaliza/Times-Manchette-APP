7f3f0feb0270cfe215bf37be6a7a843582a90bac
Graver Personaliza <lojagraver@gmail.com>
2026-04-04 17:50:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker */
-const CACHE_NAME = "manchette-player-password-v20260404c";
+const CACHE_NAME = "manchette-ui-ajustes-v6-20260404";
 const ASSETS = [
   "./",
   "./index.html",
